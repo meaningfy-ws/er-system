b11d965981302345a21e292fc993ec3310f72e77
Rashif Ray Rahman <schiv@archlinux.org>
2025-08-04 13:21:37 +0600

    refactor: more conventional abstract class, methods
    It is better to have human readable abstract class names instead of
    prepending or appending "ABC". Rename some of the methods according to
    language conventions (add/remove vs. insert/delete), including more
    understandable signatures (`entity_id` vs. `entity` where ID is
    expected). Add initial docstrings.
    
    See https://github.com/cosmicpython/code/blob/chapter_06_uow_exercise/src/allocation/adapters/repository.py

--- src/adapters/repository.py
+++ src/adapters/repository.py
@@ -1,26 +1,24 @@
 import abc
 
 
-class ABCRepository(abc.ABC):
+class AbstractRepository(abc.ABC):
 
     @abc.abstractmethod
-    def add(self, data):
-        raise NotImplementedError
+    def add(self, entity):
+        """Add an entity to the repository."""
 
     @abc.abstractmethod
-    def get_by_id(self, data):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def delete(self, data):
-        raise NotImplementedError
-
-
-    @abc.abstractmethod
-    def update(self, data):
-        raise NotImplementedError
-
+    def get(self, entity_id):
+        """Get a single entity by its ID from the repository."""
 
     @abc.abstractmethod
     def list(self):
-        raise NotImplementedError
+        """List all entities in the repository."""
+
+    @abc.abstractmethod
+    def remove(self, entity_id):
+        """Remove an entity by its ID from the repository."""
+
+    @abc.abstractmethod
+    def update(self, entity):
+        """Update the entity’s information in the repository."""
